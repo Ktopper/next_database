5127bd53ae67a0d3f99a1cfeb90e4854e4e85cb6
Ktop <ktopdunham@gmail.com>
2024-04-22 19:20:09 -0400

    Refactor file paths and imports in onboarding code

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,16 @@
 'use client'
-import { useRef } from "react";
+
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import HomePageCards from "@/components/HomePageCards";
 
 function HomePage() {
-  const heroRef = useRef(); // This will be used to reference the Hero component
+ // This will be used to reference the Hero component
 
   return (
     <div className="container">
-      <Navbar heroRef={heroRef} delay={true} />
+      <Navbar  delay={true} />
       <Hero
         title="Welcome to Fervor Books"
         statement="Where Writing meets AI adventures"
