1eb86a1a05d57728f389fdcfe4629538da883fa3
Ktop <ktopdunham@gmail.com>
2024-04-22 18:29:51 -0400

    Update firebase-config.js to include analytics initialization

--- util/firebase-config.js
+++ util/firebase-config.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
     apiKey: "YOUR_API_KEY",
@@ -7,10 +8,12 @@ const firebaseConfig = {
     projectId: "YOUR_PROJECT_ID",
     storageBucket: "YOUR_PROJECT_ID.appspot.com",
     messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    appId: "YOUR_APP_ID",
+    measurementId: "G-4X1ZQVVJ6X"
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const analytics = getAnalytics(app);
 
 export default db;
