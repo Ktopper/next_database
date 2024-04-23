68cd7c4a64a5cad97008590212f1ce612d1b1710
Ktop <ktopdunham@gmail.com>
2024-04-22 22:25:54 -0400

    Update Firebase configuration in OnboardingForm component

--- src/components/OnboardingForm.js
+++ src/components/OnboardingForm.js
@@ -29,13 +29,13 @@ function OnboardingForm() {
       const { getAnalytics } = await import('firebase/analytics');
 
       const firebaseConfig = {
-        apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-        authDomain: `${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}.firebaseapp.com`,
-        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-        storageBucket: `${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}.appspot.com`,
-        messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-        appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-        measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+        apiKey: process.env.API_KEY,
+        authDomain: `${process.env.PROJECT_ID}.firebaseapp.com`,
+        projectId: process.env.PROJECT_ID,
+        storageBucket: `${process.env.PROJECT_ID}.appspot.com`,
+        messagingSenderId: process.env.SENDER_ID,
+        appId: process.env.APP_ID,
+        measurementId: process.env.MEASUREMENT_ID,
       };
 
       const app = initializeApp(firebaseConfig);
